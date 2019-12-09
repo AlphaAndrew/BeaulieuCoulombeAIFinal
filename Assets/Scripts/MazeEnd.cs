c337148aaa22e6001423ebf3dcd56d80a300678d
AlphaAndrew <54077501+AlphaAndrew@users.noreply.github.com>
2019-12-09 14:17:52 -0500

    maze end

---
+++ Assets/Scripts/MazeEnd.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeEnd : MonoBehaviour
+{
+    private GameObject[] enemies;
+    public GameObject winScreen;
+    public GameObject loseScreen;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            enemies = GameObject.FindGameObjectsWithTag("EnemyMouse");
+            foreach(GameObject enemy in enemies)
+            {
+                Destroy(enemy);
+            }
+            winScreen.SetActive(true);
+        }
+        if (other.gameObject.CompareTag("EnemyMouse"))
+        {
+            //Turn off movement
+            loseScreen.SetActive(true);
+        }
+    }
+}
